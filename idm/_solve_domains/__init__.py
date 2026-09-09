07bd8c6664729b965ac8d0c8f6c599f175289c76
Yaoharee Lahtee <127247155+morrocwi@users.noreply.github.com>
2026-09-09 22:59:41 +0700

    Load Navier-Stokes retained solver domain

--- idm/_solve_domains/__init__.py
+++ idm/_solve_domains/__init__.py
@@ -29,3 +29,4 @@ from . import d26_p2_statistics_probability_exact  # noqa: F401
 from . import d27_p2_computational_geometry_exact  # noqa: F401
 from . import d28_p2_cryptographic_number_theory  # noqa: F401
 from . import d29_h1_hilbert_space_mathematical_c  # noqa: F401
+from . import d30_navier_stokes_retained  # noqa: F401
